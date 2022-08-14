e5715e9e6d2c4ba37c39fd2973aa4d20f0b22af1
GUchechi <godswilluchendu7@gmail.com>
2022-08-14 04:52:36 +0100

    added teinary operator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
 
+
+
 function App() {
+
+  const isDone = true;
+  const strikeThrough = { textDecoration : "line-through"}
   return (
     <div className="App">
-      
+
+      <p style={ isDone === true ? strikeThrough : null}>Buy Milk</p>
     </div>
   );
 }
